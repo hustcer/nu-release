5ab8fae7630204cb04ef831dfa8f26b32f08eb7d
hustcer <hustcer@outlook.com>
2022-07-30 14:14:35 +0800

    add bits and command

--- src/tests/test_bits.rs
+++ src/tests/test_bits.rs
@@ -6,6 +6,11 @@ fn bits_and() -> TestResult {
 }
 
 #[test]
+fn bits_and_negative() -> TestResult {
+    run_test("-3 | bits and 5", "5")
+}
+
+#[test]
 fn bits_and_list() -> TestResult {
     run_test("[1 2 3 8 9 10] | bits and 2 | str collect", "022002")
 }
