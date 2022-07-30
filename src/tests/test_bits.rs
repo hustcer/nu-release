d31f2a584e6c1abba5ff56f1bd364f30e9828f9b
hustcer <hustcer@outlook.com>
2022-07-30 16:47:21 +0800

    add bits or and xor command

--- src/tests/test_bits.rs
+++ src/tests/test_bits.rs
@@ -14,3 +14,33 @@ fn bits_and_negative() -> TestResult {
 fn bits_and_list() -> TestResult {
     run_test("[1 2 3 8 9 10] | bits and 2 | str collect", "022002")
 }
+
+#[test]
+fn bits_or() -> TestResult {
+    run_test("2 | bits or 3", "3")
+}
+
+#[test]
+fn bits_or_negative() -> TestResult {
+    run_test("-3 | bits or 5", "-3")
+}
+
+#[test]
+fn bits_or_list() -> TestResult {
+    run_test("[1 2 3 8 9 10] | bits or 2 | str collect '.'", "3.2.3.10.11.10")
+}
+
+#[test]
+fn bits_xor() -> TestResult {
+    run_test("2 | bits xor 3", "1")
+}
+
+#[test]
+fn bits_xor_negative() -> TestResult {
+    run_test("-3 | bits xor 5", "-8")
+}
+
+#[test]
+fn bits_xor_list() -> TestResult {
+    run_test("[1 2 3 8 9 10] | bits xor 2 | str collect '.'", "3.0.1.10.11.8")
+}
